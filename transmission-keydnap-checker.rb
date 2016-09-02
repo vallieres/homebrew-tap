87f92f2ad23d66b7117c71574a71cd84757ff316
Alexandre Vallières-Lagacé <alexandre@vallier.es>
2016-09-02 10:43:26 -0400

    Updated transmission-keydnap-checker to 1.0.2

--- transmission-keydnap-checker.rb
+++ transmission-keydnap-checker.rb
@@ -1,13 +1,12 @@
 class TransmissionKeydnapChecker < Formula
   desc "Simple Homebrew command to scan your system for Keydnap"
   homepage "https://github.com/vallieres/transmission-keydnap-checker/"
-  url "https://github.com/vallieres/transmission-keydnap-checker/archive/1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "5d28338ffd1c6ee0dc123f2135c0d993e0c20077c2feaf9b8d69daf51677c31b"
+  url "https://github.com/vallieres/transmission-keydnap-checker/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "131a3c9b154c8817c00f659b44a0f518c830f20f8df7fab6576704a1caa81083"
 
   def install
         bin.install "transmission-keydnap-checker"
   end
 
 end
-
